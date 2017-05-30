d91cc8e66b01d2fb9020cba08dfb7b5e3b6c4654
Brandon Wood <wood149@purdue.edu>
2017-05-30 16:52:09 -0400

    Add NASA api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import {Switch,Route} from 'react-router-dom'
+import { Switch, Route, NavLink } from 'react-router-dom'
+import Github from './Github'
+import Nasa from './Nasa'
+import Homework from './Homework'
 import './App.css';
 
 class App extends Component {
@@ -7,13 +10,27 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <div className="Appheadings">
+          <div className="App-headings">
             <h3>Ain't no party like an</h3>
             <h1>API Party</h1>
           </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to={'/github'}>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/nasa'}>NASA API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/homework'}>Homework</NavLink>
+            </li>
+          </ul>
         </div>
         <Switch>
-          <Route render={()=><p>To get started, click one of the links</p>}/>
+          <Route path='/github' component={Github} />
+          <Route path='/nasa' component={Nasa} />
+          <Route path='/homework' component={Homework} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
     );
